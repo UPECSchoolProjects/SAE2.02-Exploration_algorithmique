611bcc51136f71ecee237df967112b80bbe58508
Pika Chu <briaultmaxence@gmail.com>
2023-03-13 15:26:34 +0100

    ANTOINE PRENDS ET TG 2 (cest le debut de la class analyse)

--- sae202/src/main/java/com/uwu/analyse.java
+++ sae202/src/main/java/com/uwu/analyse.java
@@ -1,14 +1,14 @@
 package com.uwu;
 package com.uwu.Conversion;
-
 import java.io.File;
 
-public class Analyse {
+public class analyse {
     
     public phrase(test){
-        
+        File file = new File(test);
         BufferedReader br = new BufferedReader(new FileReader("test"));
         String st;
         while ((st = br.readLine()) != null);
+        return st;
     }
 }
