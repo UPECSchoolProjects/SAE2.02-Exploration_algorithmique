367a7f23129ca879e03a323b1b3e7e0f251b0355
Antoine <antjarnoux@gmail.com>
2023-03-13 15:29:47 +0100

    J'enlève la merde que maxence a mis dans App(je l'embete il est mimi)

--- sae202/src/main/java/com/uwu/App.java
+++ sae202/src/main/java/com/uwu/App.java
@@ -1,30 +1,10 @@
 package com.uwu;
 
-import java.io.File;
-import java.io.*;
 import com.uwu.Conversion.ConversionFactory;
 import com.uwu.Conversion.IConverter;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        // IConverter cf = ConversionFactory.getConverter("test.pdf");
-
-        // cf.convert();
-
-        // File test = new File("test.txt");
-        // System.out.println(test);
-
-        File test = new File("test.txt");
-        BufferedReader br = new BufferedReader(new FileReader("test.txt"));
-        String st;
-        while ((st = br.readLine()) != null)
-            System.out.println(st);
-        System.out.println("Done");
-
-        
-
-        
     }
 }
