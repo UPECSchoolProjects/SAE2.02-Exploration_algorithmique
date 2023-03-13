8fa24799bf6e5ad920bb7341d7f2b2b482a0234d
Pika Chu <briaultmaxence@gmail.com>
2023-03-13 15:16:59 +0100

    class analyse

--- sae202/src/main/java/com/uwu/analyse.java
+++ sae202/src/main/java/com/uwu/analyse.java
@@ -2,5 +2,9 @@ package com.uwu;
 
 public class Analyse {
     
-    public phrase()
+    public phrase(File test){
+        BufferedReader br = new BufferedReader(new FileReader("test"));
+        String st;
+        while ((st = br.readLine()) != null);
+    }
 }
