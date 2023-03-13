847c013a4218b8206445686c043e1aaed5775e1b
Pika Chu <briaultmaxence@gmail.com>
2023-03-13 15:23:53 +0100

    ANTOINE TG PRENDS

--- sae202/src/main/java/com/uwu/analyse.java
+++ sae202/src/main/java/com/uwu/analyse.java
@@ -1,8 +1,12 @@
 package com.uwu;
+package com.uwu.Conversion;
+
+import java.io.File;
 
 public class Analyse {
     
-    public phrase(File test){
+    public phrase(test){
+        
         BufferedReader br = new BufferedReader(new FileReader("test"));
         String st;
         while ((st = br.readLine()) != null);
