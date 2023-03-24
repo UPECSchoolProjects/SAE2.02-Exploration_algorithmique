14da76b7ea4abbb3e254fd085759af06dd40b61a
Antoine <antjarnoux@gmail.com>
2023-03-24 16:17:55 +0100

    PDFINTOTXT

--- sae202/src/main/java/com/uwu/Conversion/PdfConverter.java
+++ sae202/src/main/java/com/uwu/Conversion/PdfConverter.java
@@ -1,17 +1,40 @@
 package com.uwu.Conversion;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PdfConverter implements IConverter {
 
-    public PdfConverter(String f) {
-        // TODO Auto-generated constructor stub
+    private String filePath;
+
+    public PdfConverter(String filePath) {
+        this.filePath = filePath;
     }
 
     @Override
     public File convert() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convert'");
+        PDDocument document = null;
+        BufferedWriter writer = null;
+
+        try {
+            document = PDDocument.load(new File(filePath));
+            writer = new BufferedWriter(new FileWriter("output.txt"));
+
+            PDFTextStripper stripper = new PDFTextStripper();
+            stripper.writeText(document, writer);
+
+            writer.close();
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new File("output.txt");
     }
 
 }
