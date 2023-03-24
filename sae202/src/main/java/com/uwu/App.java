2c6edbd884674d88287db94868cb693a4c6c92da
Antoine <antjarnoux@gmail.com>
2023-03-24 15:39:04 +0100

    bug MAVEN NTM

--- sae202/src/main/java/com/uwu/App.java
+++ sae202/src/main/java/com/uwu/App.java
@@ -1,9 +1,10 @@
 package com.uwu;
 
-import com.uwu.Conversion.ConversionFactory;
-import com.uwu.Conversion.IConverter;
-import com.uwu.Conversion.ConversionFactory;
-import com.uwu.Conversion.IConverter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -13,12 +14,13 @@ import java.util.HashMap;
 public class App {
     public static void main(String[] args) throws Exception {
 
-         //HTMLConverter html = new HTMLConverter("HTMLTEST/Histoire de Neuilly par lAbbé Bellanger - Wikisource.html", "HTMLParsed/", "prp-pages-output");
-         //html.convert();
-         //HTMLConverter html = new HTMLConverter("HTMLTEST/html6.html", "HTMLParsed/");
-         
-        //html.convert();
-         
+        // HTMLConverter html = new HTMLConverter("HTMLTEST/Histoire de Neuilly par
+        // lAbbé Bellanger - Wikisource.html", "HTMLParsed/", "prp-pages-output");
+        // html.convert();
+        // HTMLConverter html = new HTMLConverter("HTMLTEST/html6.html", "HTMLParsed/");
+
+        // html.convert();
+
         // IConverter cf = ConversionFactory.getConverter("test.pdf");
 
         // cf.convert();
@@ -27,19 +29,18 @@ public class App {
         // System.out.println(test);
 
         analyse analyseur = new analyse();
-        List<String> motsVides = Arrays.asList("le", "la", "de", "du", "et", "ou", "mais", "donc", "pour", "par", "avec", "sans");
+        List<String> motsVides = Arrays.asList("le", "la", "de", "du", "et", "ou", "mais", "donc", "pour", "par",
+                "avec", "sans");
         String cheminFichier = "test.txt";
         try {
             List<Map.Entry<String, Integer>> listeFreq = analyseur.calculerFrequences(cheminFichier, motsVides);
             for (Map.Entry<String, Integer> entry : listeFreq) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+                System.out.println(entry.getKey() + " : " + entry.getValue());
             }
-    
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        
     }
 }
