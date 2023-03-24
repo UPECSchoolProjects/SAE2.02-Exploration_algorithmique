a0258891f8bf91ef5da058addbca6f444f264201
Pika Chu <briaultmaxence@gmail.com>
2023-03-24 14:42:48 +0100

    Class analyse

--- sae202/src/main/java/com/uwu/App.java
+++ sae202/src/main/java/com/uwu/App.java
@@ -2,22 +2,29 @@ package com.uwu;
 
 import com.uwu.Conversion.ConversionFactory;
 import com.uwu.Conversion.IConverter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // IConverter cf = ConversionFactory.getConverter("test.pdf");
+    analyse analyseur = new analyse();
+    List<String> motsVides = Arrays.asList("le", "la", "de", "du", "et", "ou", "mais", "donc", "pour", "par", "avec", "sans");
+    String cheminFichier = "test.txt";
+    try {
+        List<Map.Entry<String, Integer>> listeFreq = analyseur.calculerFrequences(cheminFichier, motsVides);
+        for (Map.Entry<String, Integer> entry : listeFreq) {
+        System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
-        // cf.convert();
-
-        // File test = new File("test.txt");
-        // System.out.println(test);
-
-        analyse text=new analyse();
-        text.phrase("test.txt");
-
-
-
-        
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+ 
     }
 }
--- sae202/src/main/java/com/uwu/analyse.java
+++ sae202/src/main/java/com/uwu/analyse.java
@@ -1,21 +1,39 @@
 package com.uwu;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 public class analyse {
-    
-    public String phrase(String test) {
-        // File file = new File(test);
-        String text = readFile(test);
-        return text;
-    }
 
-    public String readFile(String fileName) {
-        BufferedReader br = new BufferedReader(new FileReader(Filename text));
-        String st;
-        while ((st = br.readLine()) != null) {
-            System.out.println(st);
+    public List<Map.Entry<String, Integer>> calculerFrequences(String cheminFichier, List<String> motsVides) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(cheminFichier));
+        String ligne;
+        StringBuilder sb = new StringBuilder();
+        while ((ligne = br.readLine()) != null) {
+            sb.append(ligne).append(" ");
         }
         br.close();
-        return st;
+
+        // Diviser le texte en mots et compter leur fréquence
+        Map<String, Integer> freq = new HashMap<>();
+        String[] mots = sb.toString().split("\\W+");
+        for (String mot : mots) {
+            if (!motsVides.contains(mot)) {
+                freq.put(mot, freq.getOrDefault(mot, 0) + 1);
+            }
+        }
+
+        // Convertir la carte de fréquence en liste triée par ordre décroissant de fréquence
+        List<Map.Entry<String, Integer>> listeFreq = new ArrayList<>(freq.entrySet());
+        listeFreq.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+        return listeFreq;
     }
 }
+
