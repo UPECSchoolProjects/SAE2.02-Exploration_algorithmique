7f556c2c7124c38107e1b580a9e34d5fa13d1ed3
Antoine <antjarnoux@gmail.com>
2023-03-29 11:54:06 +0200

    Truc que Maxence a rage quit

--- sae202/src/main/java/com/uwu/Analyse.java
+++ sae202/src/main/java/com/uwu/Analyse.java
@@ -12,6 +12,29 @@ import java.util.Map;
 
 public class Analyse {
 
+    static private java.util.Map<String, String> exceptedMap = java.util.Map.ofEntries(
+            java.util.Map.entry("é", "e"),
+            java.util.Map.entry("è", "e"),
+            java.util.Map.entry("ê", "e"),
+            java.util.Map.entry("à", "a"),
+            java.util.Map.entry("â", "a"),
+            java.util.Map.entry("î", "i"),
+            java.util.Map.entry("ï", "i"),
+            java.util.Map.entry("ô", "o"),
+            java.util.Map.entry("û", "u"),
+            java.util.Map.entry("ù", "u"),
+            java.util.Map.entry("ç", "c"),
+            java.util.Map.entry("œ", "oe"),
+            java.util.Map.entry("æ", "ae"),
+            java.util.Map.entry("ë", "e"),
+            java.util.Map.entry("ü", "u"),
+            java.util.Map.entry("ÿ", "y"),
+            java.util.Map.entry("Æ", "Ae"),
+            java.util.Map.entry("Ø", "Oe"),
+            java.util.Map.entry("Å", "Aa"),
+            java.util.Map.entry("Þ", "Th"),
+            java.util.Map.entry("ß", "ss"),
+            java.util.Map.entry("Æ", "Ae"));
     ArrayList<String> motsVides = new ArrayList<String>();
     private String filePath;
 
@@ -35,31 +58,41 @@ public class Analyse {
         }
     }
 
-    public Map<String, Integer> calculerFrequences() throws IOException { // Renvoie une Map avec les mots et leur
-                                                                          // fréquence
+    public Map<String, Integer> calculerFrequences() throws IOException { // Renvoie une Map avec les mots et leur fréquence
         Map<String, Integer> freq = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) { // Ouvre le fichier
             String ligne;
-            while ((ligne = br.readLine()) != null) { // Lit le fichier ligne par ligne
-                String[] mots = ligne.split("\\W+"); // Sépare la ligne en mots
+            while ((ligne = br.readLine()) != null) {           // Lit le fichier ligne par ligne
+                String[] mots = ligne.split("\W+");      // Sépare la ligne en mots
                 for (String mot : mots) {
-                    mot = mot.toLowerCase();  minuscule
-                    char[] mot_array = mot.toCharArray();               
-                    if (!motsVides.contains(mot) && mot.length() > 1)              // Vérifie que le mot n'est pas vide
+                    mot = mot.toLowerCase(); // Met le mot en minuscule
+                    mot = mot.replaceAll("[^a-z]", ""); // Supprime les caractères non alphabétiques
+                    mot_array = mot.toCharArray(); // Convertit le mot en tableau de caractères
+                    for (int i = 0; i < mot_array.length; i++) { // Parcourt le tableau de caractères
+                        if (exceptedMap.containsKey(mot_array[i])) { // Vérifie si le caractère est dans la Map
+                            mot_array[i] = exceptedMap.get(mot_array[i]).charAt(0); // Remplace le caractère
+                        }
+                    }
+                    mot = new String(mot_array); // Convertit le tableau de caractères en String
+                    if (!motsVides.contains(mot) && mot.length() > 1) {             // Vérifie que le mot n'est pas vide
                         freq.put(mot, freq.getOrDefault(mot, 0) + 1);  // Incrémente la fréquence du mot
                     }
                 }
             }
         }
 
-     
+        return freq;
+    }
 
-     
-                                                                  // 
-    publi
+    //
+    public void ecrireCSV(String nomFichier) throws IOException { // Génère un fichier CSV avec les mots et leur
+                                                                  // fréquence
+        try (FileWriter writer = new FileWriter(nomFichier)) {
+            writer.append("MOT");
+            writer.append(";");
+            writer.append("FREQUENCE");
+            writer.append("\n");
 
-    
-            
             Map<String, Integer> freq = calculerFrequences();
             for (Map.Entry<String, Integer> entry : freq.entrySet()) { // Parcourt la Map
                 writer.append(entry.getKey());
@@ -68,6 +101,5 @@ public class Analyse {
                 writer.append("\n");
             }
         }
- 
-
-
+    }
+}
