7e6107e3f28a40fba42ba71cce717266cedc773a
WinteruOfficiel <Winteru@the-nwo.fun>
2023-03-29 11:36:52 +0200

    changement test pour catch

--- sae202/src/main/java/com/uwu/analyse.java
+++ sae202/src/main/java/com/uwu/analyse.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Analyse {
@@ -45,7 +43,8 @@ public class Analyse {
             while ((ligne = br.readLine()) != null) {           // Lit le fichier ligne par ligne
                 String[] mots = ligne.split("\\W+");      // Sépare la ligne en mots
                 for (String mot : mots) {
-                    mot = mot.toLowerCase();                   // Met le mot en minuscule
+                    mot = mot.toLowerCase(); 
+                                      // Met le mot en minuscule
                     if (!motsVides.contains(mot) && mot.length() > 1) {             // Vérifie que le mot n'est pas vide
                         freq.put(mot, freq.getOrDefault(mot, 0) + 1);  // Incrémente la fréquence du mot
                     }
