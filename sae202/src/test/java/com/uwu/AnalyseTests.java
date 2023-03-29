9386b89a8cd978d9792f8f06e603f825c87c34ae
WinteruOfficiel <Winteru@the-nwo.fun>
2023-03-29 12:12:19 +0200

    Correction bug test (mot interdit dans test)

--- sae202/src/test/java/com/uwu/AnalyseTests.java
+++ sae202/src/test/java/com/uwu/AnalyseTests.java
@@ -12,17 +12,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AnalyseTests 
-{
-    Map<String, Integer> exceptedMap = Map.ofEntries(
-        Map.entry("je", 2),
-        Map.entry("vais", 2)
-    );
+public class AnalyseTests {
+    Map<String, Integer> exceptedMap = Map.ofEntries(Map.entry("pomme", 2), Map.entry("arbre", 2));
 
 
     @Test
-    public void verifyAnalyse()
-    {
+    public void verifyAnalyse() {
         Analyse analyseur = new Analyse("TestsInputs/testFrequence.txt");
 
         Map<String, Integer> map;
