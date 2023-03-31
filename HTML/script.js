700dd2f428dc45c1dc0b75feacdfc9af5249e061
WinteruOfficiel <Winteru@the-nwo.fun>
2023-03-31 13:12:01 +0200

    html changed

--- HTML/script.js
+++ HTML/script.js
@@ -1,4 +1,6 @@
-console.log("Hello World")
+const HOST = "http://localhost:3000";
+let chart = null;
+let nbwords = 15;
 
 async function fetchAsync(url) {
     // fonction pour récuperer un fichier sur le serveur
@@ -7,8 +9,6 @@ async function fetchAsync(url) {
     console.log("Code :" + response.status)
     return data;
 }
-
-fetchAsync("http://localhost:3000/frequences.csv")
 
 async function CSVToArray(strData, strDelimiter) {
     // fonction pour convertir un fichier csv en tableau
@@ -37,23 +37,111 @@ function chartSize(data, nbwords) {
     return data
 }
 
-anychart.onDocumentReady(async function () {
+async function getCSVData(dataURL, nbwords) {
+    console.debug(dataURL)
+    let data = await CSVToArray(await fetchAsync(dataURL));
+    return chartSize(data, nbwords);
+}
+
+async function drawChart(dataURL, nbwords) {
+    console.log(dataURL)
     // fonction pour créer le nuage de mots
-    var data = await CSVToArray(await fetchAsync("http://localhost:3000/frequences.csv"));
-    data = chartSize(data, 500);
     // create a tag (word) cloud chart
-    var chart = anychart.tagCloud(data);
+    chart = anychart.tagCloud(await getCSVData(dataURL, nbwords));
 
     // set a chart title
     chart.title('Nuage de mots')
     // set an array of angles at which the words will be laid out
     chart.angles([0, 10, -10])
     // enable a color range
-    chart.colorRange(true);
+    //chart.colorRange(true);
     // set the color range length
     chart.tooltip().format(`Le mot "{%x}" apparait {%freq}% des fois ({%value} fois)`);
 
     // display the word cloud chart
     chart.container("container");
     chart.draw();
+}
+
+async function populateSelect() {
+    // fonction pour remplir le selecteur de fichier
+    let select = document.getElementById("selector");
+    try {
+        data = await fetchAsync(HOST + "/?all=true")
+        let files = JSON.parse(data).filesObj;
+
+        //groupBy folder
+        let folders = new Map();
+        for (file of files) {
+            let folder = file.folder;
+            if (!folders.has(folder)) {
+                folders.set(folder, []);
+            }
+            folders.get(folder).push(file);
+        }
+
+        // create optionGroup and put files inside
+        for (let [folder, files] of folders) {
+            let optGroup = document.createElement("optgroup");
+            optGroup.label = folder;
+
+            for (file of files) {
+                let opt = document.createElement("option");
+                let filename = file.filename;
+                if (!filename.endsWith(".csv")) continue;
+                opt.value = filename;
+                opt.innerHTML = filename.substring(0, filename.length - 4);
+                optGroup.appendChild(opt);
+            }
+
+            if (optGroup.children.length > 0) {
+                select.appendChild(optGroup);
+            }
+        }
+    } catch (e) {
+        console.error(e)
+        let opt = document.createElement("option");
+        opt.value = "error";
+        opt.innerHTML = "Couldn't load files...";
+        select.appendChild(opt);
+    }
+}
+
+function getSelectedFile() {
+    // fonction pour récuperer le fichier sélectionné
+    let select = document.getElementById("selector");
+    let selectedOption = select.selectedIndex == -1 ? 0 : select.selectedIndex;
+    let file = select.options[selectedOption].value;
+    console.log(file)
+
+    let folder = select.options[selectedOption].parentNode.label
+    if(!folder.endsWith("/")) folder += "/";
+
+    return {filename: file, folder: folder};
+}
+
+async function changeFile() {
+    // fonction pour changer le fichier affiché
+    console.log("changed file to " + document.getElementById("selector").value + " !");
+    let file = getSelectedFile();
+    chart.data(await getCSVData(HOST + file.folder + file.filename, nbwords));
+}
+
+async function ready() {
+    // fonction pour lancer le nuage de mots
+    await populateSelect();
+    let path = getSelectedFile();
+
+    drawChart(HOST + path.folder + path.filename, 15);
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+document.getElementById("selector").addEventListener("change", changeFile);
+document.getElementById("nbwords").addEventListener("change", ($event) => {
+    let value = $event.target.value;
+    console.log(value)
+    if(isNaN(value) || value == "") return;
+    console.log("changed nbwords to " + value + " !");
+    nbwords = value;
+    changeFile();
 });
--- Serveur/index.js
+++ Serveur/index.js
@@ -6,17 +6,54 @@ const bodyParser = require('body-parser');
 const config = require('./config/config.json');
 const httpsConfig = require('./config/httpsConfig.json');
 const cors = require('cors');
+const fs = require("fs");
 
 console.log(result);
 
 const port = process.env.PORT || config.port || 3000;
+const directory = process.env.DIRECTORY || config.directory || 'files';
+const directoryPath = path.resolve(__dirname, directory);
 
 const srv = express();
 
 srv.use(cors());
 srv.use(bodyParser.json({ limit: '100mb', extended: true }))
 srv.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
-srv.use(express.static('files'))
+srv.use(express.static(directoryPath))
+
+srv.get('/', async (req, res) => {
+    let folder = req.query.folder || "";
+    let allOption = req.query.all || false;
+    console.log(allOption)
+    // list all files in the files folder
+    try {
+        let files = await fs.readdirSync(path.resolve(directoryPath, folder));
+        let filesObj = [];
+        for (let i = 0; i < files.length; i++) {
+            const stats = await fs.statSync(path.resolve(directoryPath, folder, files[i]));
+
+            filesObj.push({ filename: files[i], isDir: stats.isDirectory(), folder: "/" })
+        }
+        if (allOption) {
+            console.log("allOption if")
+            for (file of filesObj) {
+                console.log("file :", file)
+                if (file.isDir) {
+                    console.log(file.filename)
+                    let files = await fs.readdirSync(path.resolve(directoryPath, folder, file.filename));
+                    for (fileChild of files) {
+                        const stats = await fs.statSync(path.resolve(directoryPath, folder, file.filename, fileChild));
+                        filesObj.push({ filename: fileChild, isDir: stats.isDirectory(), folder: "/" + file.filename })
+                    }
+                }
+            }
+        }
+        res.status(200).json({ filesObj });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
 
 
 if (httpsConfig.useNGINX) {
