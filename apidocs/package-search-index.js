6c0fe91901891ebfdd8f3929f190fe95fa95f0aa
WinteruOfficiel <Winteru@the-nwo.fun>
2023-04-02 04:00:04 +0200

    generate javadoc

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.uwu"},{"l":"com.uwu.Conversion"},{"l":"com.uwu.Conversion.HTMLParser"},{"l":"com.uwu.Stemming"}];updateSearchResults();
